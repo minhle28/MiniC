abf49bd26edbdb310e4707fbc4908c8f35b31f5f
laphunhon <laphunhon@gmail.com>
2023-11-14 13:09:08 -0500

    fix bug tokenKry reconigze as ID

--- MiniCLexer.py
+++ MiniCLexer.py
@@ -14,18 +14,24 @@ tokens = (
     'WHILE',
 )
 
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
 t_ASSIGN = r'='
 t_SEMI = r';'
 t_IF = r'if'
 t_WHILE = r'while'
 t_OPTR = r'>=|<=|==|>|<|\+|\-|\*'
 
+ID_to_tokenKey = {
+		'while' : 'WHILE',	
+		'if'		: 'IF',	
+		}
+
 def t_ID(t):
     r'[a-zA-Z][_a-zA-Z0-9]*'
+    t.type = ID_to_tokenKey.get( t.value, 'ID' )
     return t
 
 def t_NUM(t):
@@ -41,6 +47,7 @@ t_ignore = ' \t'
 
 def t_error(t):
     print(f"Unrecognized character '{t.value[0]}'")
-    t.lexer.skip(1)
+    #t.lexer.skip(1)
+    return t
 
 lexer = lex.lex()
--- MiniCParser.py
+++ MiniCParser.py
@@ -1,21 +1,27 @@
 import ply.yacc as yacc
 from MiniCLexer import tokens
 
+# Parsing rules
 def p_program(p):
     'program : stmtlist'
     p[0] = p[1]
     #print('program:', p[0])
 
-def p_stmt_block(p):
+def p_stmt_expr_semi_assign(p):
+    'stmt : ID ASSIGN expr SEMI'
+    p[0] = [['id', p[1]], '=', p[3]]
+    #print('stmt_expr_semi_assign:', p[0])
+
+def p_stmt_brace(p):
     'stmt : LBRACE stmtlist RBRACE'
     p[0] = p[2]
-    #print('stmt_block:', p[0])
+    #print('stmtlist_brace:', p[0])
 
 def p_stmt_if(p):
-    'stmt : IF LPAREN expr RPAREN stmt'
-    p[0] = ['if', [p[3], p[5]]]
-    print('stmt_if:', p[0])
-
+    'stmt : IF LPAREN expr RPAREN stmt'    
+    p[0] = ['if', p[3], p[5]]   
+    #print('stmt_if:', p)
+    
 def p_stmt_while(p):
     'stmt : WHILE LPAREN expr RPAREN stmt'
     p[0] = ['while', p[3], p[5]]
@@ -41,26 +47,18 @@ def p_expr_num(p):
     p[0] = ['num', p[1]]
     #print('expr_num:', p[0])
 
-def p_stmt_expr_semi_assign(p):
-    'stmt : ID ASSIGN expr SEMI'
-    p[0] = [['id', p[1]], '=', p[3]]
-    #print('stmt_expr_semi_assign:', p[0])
-
-def p_stmt_expr_semi_num(p):
-    'stmt : ID ASSIGN NUM SEMI'
-    p[0] = [['id', p[1]], '=', ['num', p[3]]]
-    print('stmt_expr_semi_num:', p[0])
-
 def p_expr_optr(p):
     'expr : expr OPTR expr'
     p[0] = [p[1], ['optr', p[2]], p[3]]
     #print('expr_optr:', p[0])
 
+# Error rule for syntax errors
 def p_error(p):
     if p:
-        print(f"Syntax error at token '{p.value}' on line {p.lineno}")
+        print(f"Syntax error at token '{p}' on line {p.lineno}")
     else:
         print("Syntax error: unexpected end of input")
 
 
+# Build the parser
 parser = yacc.yacc()
